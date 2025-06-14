c73d488148718c87ed28b611cc40cad118735f75
Rankela Venkata Arjun <venkataarjun.r@gmail.com>
2025-06-14 10:45:16 +0530

    config update

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/time/', // 👈 this must match your repo name
   plugins: [react()],
-})
+});
+
